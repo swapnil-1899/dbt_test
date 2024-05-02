f39a6a43e7547c7cdbb8388c3277c93879eaf6d4
Swapnil Baban Manchare <Swapnil.Manchare@exlservice.com>
2024-05-02 08:21:49 +0000

    modified with creating base cte

--- models/staging/stg_music__albums.sql
+++ models/staging/stg_music__albums.sql
@@ -1,5 +1,9 @@
+with albums as(
+    select * from {{ source('music', 'album') }}
+)
+
 select 
     albumid as album_id,
     title as album_title,
     artistid as artist_id
-from {{ source('music', 'album') }}
+from albums
